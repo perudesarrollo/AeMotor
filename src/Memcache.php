0fc823b6af239395778b3584c905b39261bb890f
Mac - Max Palomino <maxpalominomendoza@gmail.com>
2018-06-18 16:18:46 -0500

    Fix class Memcache

--- src/Memcache.php
+++ src/Memcache.php
@@ -1,73 +1,46 @@
 <?php
 namespace perudesarrollo\AeMotor;
 
-use Memcached;
-use perudesarrollo\AeMotor\Exception;
-
 class Memcache
 {
-    private $config;
-    private $m;
-    private $client_type;
-    private $ci;
     protected $errors = [];
 
-    public function __construct($config = [])
+    private $ci;
+
+    private $client_type;
+
+    private $config;
+
+    private $m;
+
+    public function __construct($config)
     {
         if (!is_array($config)) {
             throw new Exception('Memcached Library: empty config to array');
         }
 
-        $this->config = $config;
-
         // Lets try to load Memcache or Memcached Class
-        $this->client_type = class_exists($this->config['config']['engine']) ? $this->config['config']['engine'] : false;
+        $this->client_type = class_exists($config['config']['engine']) ? $config['config']['engine'] : false;
 
         if ($this->client_type) {
             // Which one should be loaded
             switch ($this->client_type) {
                 case 'Memcached':
-                    $this->m = new Memcached();
+                    $this->m = new \Memcached();
                     break;
                 case 'Memcache':
-                    $this->m = new Memcache();
+                    $this->m = new \Memcache();
                     // Set Automatic Compression Settings
-                    if ($this->config['config']['auto_compress_tresh']) {
-                        $this->setcompressthreshold($this->config['config']['auto_compress_tresh'], $this->config['config']['auto_compress_savings']);
+                    if ($config['config']['auto_compress_tresh']) {
+                        $this->setcompressthreshold($config['config']['auto_compress_tresh'], $config['config']['auto_compress_savings']);
                     }
                     break;
             }
+            $this->config = $config;
             $this->auto_connect();
         } else {
             throw new Exception('Memcached Library: Failed to load Memcached or Memcache Class');
         }
-    }
-
-    /**
-     * Name: auto_connect
-     * Purpose: runs through all of the servers defined in
-     * the configuration and attempts to connect to each
-     * @return none
-     */
-    private function auto_connect()
-    {
-        foreach ($this->config['servers'] as $key => $server) {
-            if (!$this->add_server($server)) {
-                $this->errors[] = "Memcached Library: Could not connect to the server named $key";
-                throw new Exception('Memcached Library: Could not connect to the server named "' . $key . '"');
-            }
-        }
-    }
-
-    /**
-     * Name: add_server
-     * @return TRUE or FALSE
-     */
-    public function add_server($server)
-    {
-        extract($server);
-
-        return $this->m->addServer($host, $port, $weight);
     }
 
     /*
@@ -106,40 +79,70 @@ class Memcache
         }
     }
 
+    /**
+     * Name: add_server
+     * @return TRUE or FALSE
+     */
+    public function add_server($server)
+    {
+        extract($server);
+
+        return $this->m->addServer($host, $port, $weight);
+    }
+
     /*
     +-------------------------------------+
-    Name: set
-    Purpose: similar to the add() method but uses set
-    @param return : TRUE or FALSE
+    Name: decrement
+    Purpose: decrements a value
+    @param return : none
     +-------------------------------------+
      */
 
-    public function set($key = null, $value = null, $expiration = null)
+    public function decrement($key = null, $by = 1)
     {
-        if (is_null($expiration)) {
-            $expiration = $this->config['config']['expiration'];
+        return $this->m->decrement($this->key_name($key), $by);
+    }
+
+    /*
+    +-------------------------------------+
+    Name: delete
+    Purpose: deletes a single or multiple data elements from the memached servers
+    @param return : none
+    +-------------------------------------+
+     */
+
+    public function delete($key, $expiration = null)
+    {
+        if (is_null($key)) {
+            $this->errors[] = 'The key value cannot be NULL';
+
+            return false;
         }
+
+        if (is_null($expiration)) {
+            $expiration = $this->config['config']['delete_expiration'];
+        }
+
         if (is_array($key)) {
             foreach ($key as $multi) {
-                if (!isset($multi['expiration']) || '' == $multi['expiration']) {
-                    $multi['expiration'] = $this->config['config']['expiration'];
-                }
-                $this->set($this->key_name($multi['key']), $multi['value'], $multi['expiration']);
+                $this->delete($multi, $expiration);
             }
         } else {
-            switch ($this->client_type) {
-                case 'Memcache':
-                    $add_status = $this->m->set($this->key_name($key), $value, $this->config['config']['compression'], $expiration);
-                    break;
-
-                default:
-                case 'Memcached':
-                    $add_status = $this->m->set($this->key_name($key), $value, $expiration);
-                    break;
-            }
-
-            return $add_status;
+            return $this->m->delete($this->key_name($key), $expiration);
         }
+    }
+
+    /*
+    +-------------------------------------+
+    Name: flush
+    Purpose: flushes all items from cache
+    @param return : none
+    +-------------------------------------+
+     */
+
+    public function flush()
+    {
+        return $this->m->flush();
     }
 
     /*
@@ -175,30 +178,70 @@ class Memcache
 
     /*
     +-------------------------------------+
-    Name: delete
-    Purpose: deletes a single or multiple data elements from the memached servers
+    Name: getstats
+    Purpose: Get Server Stats
+    Possible: "reset, malloc, maps, cachedump, slabs, items, sizes"
+    @param returns an associative array with server's statistics. Array keys correspond to stats parameters and values to parameter's values.
+    +-------------------------------------+
+     */
+
+    public function getstats($type = 'items')
+    {
+        switch ($this->client_type) {
+            case 'Memcache':
+                $stats = $this->m->getStats($type);
+                break;
+
+            default:
+            case 'Memcached':
+                $stats = $this->m->getStats();
+                break;
+        }
+
+        return $stats;
+    }
+
+    /*
+    +-------------------------------------+
+    Name: getversion
+    Purpose: Get Server Vesion Number
+    @param Returns a string of server version number or FALSE on failure.
+    +-------------------------------------+
+     */
+
+    public function getversion()
+    {
+        return $this->m->getVersion();
+    }
+
+    /*
+    +-------------------------------------+
+    Name: increment
+    Purpose: increments a value
     @param return : none
     +-------------------------------------+
      */
 
-    public function delete($key, $expiration = null)
+    public function increment($key = null, $by = 1)
     {
-        if (is_null($key)) {
-            $this->errors[] = 'The key value cannot be NULL';
+        return $this->m->increment($this->key_name($key), $by);
+    }
 
-            return false;
-        }
+    /*
+    +--------------------------------------+
+    Name: isConected
+    Purpose: Check if the memcache server is connected.
+    +--------------------------------------+
+     */
 
-        if (is_null($expiration)) {
-            $expiration = $this->config['config']['delete_expiration'];
-        }
-
-        if (is_array($key)) {
-            foreach ($key as $multi) {
-                $this->delete($multi, $expiration);
+    public function isConnected()
+    {
+        foreach ($this->getstats() as $key => $server) {
+            if (-1 == $server['pid']) {
+                return false;
             }
-        } else {
-            return $this->m->delete($this->key_name($key), $expiration);
+
+            return true;
         }
     }
 
@@ -240,79 +283,38 @@ class Memcache
 
     /*
     +-------------------------------------+
-    Name: increment
-    Purpose: increments a value
-    @param return : none
+    Name: set
+    Purpose: similar to the add() method but uses set
+    @param return : TRUE or FALSE
     +-------------------------------------+
      */
 
-    public function increment($key = null, $by = 1)
+    public function set($key = null, $value = null, $expiration = null)
     {
-        return $this->m->increment($this->key_name($key), $by);
-    }
-
-    /*
-    +-------------------------------------+
-    Name: decrement
-    Purpose: decrements a value
-    @param return : none
-    +-------------------------------------+
-     */
-
-    public function decrement($key = null, $by = 1)
-    {
-        return $this->m->decrement($this->key_name($key), $by);
-    }
-
-    /*
-    +-------------------------------------+
-    Name: flush
-    Purpose: flushes all items from cache
-    @param return : none
-    +-------------------------------------+
-     */
-
-    public function flush()
-    {
-        return $this->m->flush();
-    }
-
-    /*
-    +-------------------------------------+
-    Name: getversion
-    Purpose: Get Server Vesion Number
-    @param Returns a string of server version number or FALSE on failure.
-    +-------------------------------------+
-     */
-
-    public function getversion()
-    {
-        return $this->m->getVersion();
-    }
-
-    /*
-    +-------------------------------------+
-    Name: getstats
-    Purpose: Get Server Stats
-    Possible: "reset, malloc, maps, cachedump, slabs, items, sizes"
-    @param returns an associative array with server's statistics. Array keys correspond to stats parameters and values to parameter's values.
-    +-------------------------------------+
-     */
-
-    public function getstats($type = 'items')
-    {
-        switch ($this->client_type) {
-            case 'Memcache':
-                $stats = $this->m->getStats($type);
-                break;
-
-            default:
-            case 'Memcached':
-                $stats = $this->m->getStats();
-                break;
+        if (is_null($expiration)) {
+            $expiration = $this->config['config']['expiration'];
         }
+        if (is_array($key)) {
+            foreach ($key as $multi) {
+                if (!isset($multi['expiration']) || '' == $multi['expiration']) {
+                    $multi['expiration'] = $this->config['config']['expiration'];
+                }
+                $this->set($this->key_name($multi['key']), $multi['value'], $multi['expiration']);
+            }
+        } else {
+            switch ($this->client_type) {
+                case 'Memcache':
+                    $add_status = $this->m->set($this->key_name($key), $value, $this->config['config']['compression'], $expiration);
+                    break;
 
-        return $stats;
+                default:
+                case 'Memcached':
+                    $add_status = $this->m->set($this->key_name($key), $value, $expiration);
+                    break;
+            }
+
+            return $add_status;
+        }
     }
 
     /*
@@ -338,6 +340,22 @@ class Memcache
         return $setcompressthreshold_status;
     }
 
+    /**
+     * Name: auto_connect
+     * Purpose: runs through all of the servers defined in
+     * the configuration and attempts to connect to each
+     * @return none
+     */
+    private function auto_connect()
+    {
+        foreach ($this->config['servers'] as $key => $server) {
+            if (!$this->add_server($server)) {
+                $this->errors[] = "Memcached Library: Could not connect to the server named $key";
+                throw new Exception('Memcached Library: Could not connect to the server named "' . $key . '"');
+            }
+        }
+    }
+
     /*
     +-------------------------------------+
     Name: key_name
@@ -349,23 +367,5 @@ class Memcache
     private function key_name($key)
     {
         return strtolower($this->config['config']['prefix'] . ':' . $key);
-    }
-
-    /*
-    +--------------------------------------+
-    Name: isConected
-    Purpose: Check if the memcache server is connected.
-    +--------------------------------------+
-     */
-
-    public function isConnected()
-    {
-        foreach ($this->getstats() as $key => $server) {
-            if (-1 == $server['pid']) {
-                return false;
-            }
-
-            return true;
-        }
     }
 }
